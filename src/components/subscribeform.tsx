117520a7d583efc337142cf040c7ae499147b55e
luthersharp <sharptrafficagency@gmail.com>
2024-06-07 14:32:17 +0100

    Update subscribeform.tsx

--- src/components/subscribeform.tsx
+++ src/components/subscribeform.tsx
@@ -10,7 +10,7 @@ const SubscribeForm = () => {
   
   return (
    
-      <Link  className="block text-center lg:text-xl w-full mt-3 font-bold text-slate-900 rounded-md py-1  lg:py-2 lg:px-5 bg-teal-400 hover:bg-teal-300 border border-slate-900 sm:w-auto sm:mt-0" href={"https://form.feathery.io/to/EjtmXB?_id=0cb3a7e9-cac3-4b74-9377-5fa4419de8d4#sign-up"}>
+      <Link  className="block text-center lg:text-xl w-full mt-3 font-bold text-slate-900 rounded-md py-1  lg:py-2 lg:px-5 bg-teal-400 hover:bg-teal-300 border border-slate-900 sm:w-auto sm:mt-0" href={"https://form.feathery.io/to/EjtmXB"}>
         Waitlist
       </Link>
 
